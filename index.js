c881a8cfc48a3ec59699c5cea082f77e1fbd0e48
Colby Aley <colby@aley.me>
2016-07-13 13:51:34 -0700

    Lock Reflect.js to v0.1.58

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /* jshint node: true */
 'use strict';
 
-const REFLECT_JAVASCRIPT = 'https://cdn.reflect.io/latest/reflect.js',
-      REFLECT_CSS = 'https://cdn.reflect.io/latest/reflect.css';
-
+const REFLECT_JS_VERSION = '0.1.58',
+      REFLECT_JAVASCRIPT = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION +'/reflect.js',
+      REFLECT_CSS = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION + '/reflect.css';
 
 const HEAD = '<script src="' + REFLECT_JAVASCRIPT + '" type="text/javascript"></script> \
 <link rel="stylesheet" href="' + REFLECT_CSS + '">';
