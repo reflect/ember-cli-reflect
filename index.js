5445265dda092ef114c92a414e7d3692cf062bc2
Colby Aley <colby@aley.me>
2016-07-18 14:45:58 -0700

    Allow user to define their own css source

--- index.js
+++ index.js
@@ -2,15 +2,28 @@
 'use strict';
 
 const REFLECT_JS_VERSION = '0.1.59',
-      REFLECT_JAVASCRIPT = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION +'/reflect.js',
-      REFLECT_CSS = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION + '/reflect.css';
+      REFLECT_JAVASCRIPT = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION +'/reflect.js';
 
-const HEAD = '<script src="' + REFLECT_JAVASCRIPT + '" type="text/javascript"></script> \
-<link rel="stylesheet" href="' + REFLECT_CSS + '">';
+const getHead = function(config) {
+  let cssSource;
+
+  if (config.reflect && config.reflect.css) {
+    cssSource = config.reflect.css;
+  } else {
+    cssSource = 'https://cdn.reflect.io/' + REFLECT_JS_VERSION + '/reflect.css';
+  }
+
+  return '<script src="' + REFLECT_JAVASCRIPT + '" type="text/javascript"></script> \
+<link rel="stylesheet" href="' + cssSource + '">';
+};
 
 module.exports = {
   name: 'ember-cli-reflect',
   contentFor: function(type) {
-    if (type === 'head') { return HEAD };
+    if (type === 'head') {
+      let config = this.config();
+
+      return getHead(config);
+    };
   }
 };
