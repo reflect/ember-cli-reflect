3899f6bbd3dfdad9b48076f28796cfcaeb5176e5
Colby Aley <colby@aley.me>
2016-08-15 15:57:37 -0700

    Demonstrate using events to navigate from list view

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,22 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set('project', 'reflect-productivity');
-    controller.set('viewName', 'simple-dashboard-view');
+    controller.set('viewName', 'list');
     controller.set('token', 'c77b12dd-a370-48be-9b94-ee388b14510c');
 
     // timeseries is the slug of the component we're targeting.
     // legendItemClick is the event we're going to handlers for.
     let events = {
-      timeseries: {
-        legendItemClick: [
+      list: {
+        listItemClick: [
           (data) => {
             // This will change our controller viewName value, thus
             // changing the Reflect view that gets rendered.
-            controller.set('viewName', 'even-simpler-dashboard');
+            controller.set('viewName', 'simple-dashboard-view');
 
             // This will set the filters to filter for the series we clicked on.
             controller.set('filters', [
-              {field: data.field, op: '=', value: data.value, removable: true}
+              {field: 'Name', op: '=', value: data.rowData.Name, removable: true}
             ]);
           }
         ]
