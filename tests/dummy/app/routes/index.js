023931970da92d3bd74becea8cc4d31dd391828c
Colby Aley <colby@aley.me>
2016-08-25 18:23:39 -0700

    Example of using overrides

--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set('project', 'reflect-productivity');
-    controller.set('viewName', 'list');
+    controller.set('viewName', 'simple-dashboard-view');
     controller.set('token', 'c77b12dd-a370-48be-9b94-ee388b14510c');
 
     // list is the slug of the component we're targeting.
     // listItemClick is the event we're going to handlers for.
     let events = {
-      list: {
-        listItemClick: [
+      timeseries: {
+        dataPointClick: [
           (data) => {
             // This will change our controller viewName value, thus
             // changing the Reflect view that gets rendered.
-            controller.set('viewName', 'simple-dashboard-view');
+            controller.set('viewName', 'list');
 
             // Filter on the name field of the selected card.
             controller.set('filters', [
@@ -24,6 +24,11 @@ export default Ember.Route.extend({
         ]
       }
     };
+
+    // Example of using overrides to set the title on a component given its slug
+    controller.set('overrides', [
+      { slug: 'timeseries', path: 'title', value: 'My Overwritten Title' }
+    ])
 
     // We need to pass these event handlers to the Reflect component.
     controller.set('events', events);
